3c7998d47df49432724211e4b5b78c1442828a8c
Stephen Fisher <safisher@sas.upenn.edu>
2013-08-26 12:09:15 -0400

    updated STATS
    added option to specify RUM or STAR (default).
    properly handles STAR stats.
    properly handles new TRIM module.

--- ngs_STATS.sh
+++ ngs_STATS.sh
@@ -15,7 +15,7 @@
 # under the License.
 
 ##########################################################################################
-# INPUT: $SAMPLE/blast/speciesCounts.txt, $SAMPLE/trimAdapters.stats.txt, $SAMPLE/trimPolyAT.stats.txt, $SAMPLE/rum.trim/mappings_stats.txt
+# INPUT: $SAMPLE/blast/speciesCounts.txt, $SAMPLE/trim/stats.txt, $SAMPLE/star/Log.final.out
 # OUTPUT: printing to console
 ##########################################################################################
 
@@ -23,14 +23,25 @@
 # USAGE
 ##########################################################################################
 
-ngsUsage_STATS="Usage: `basename $0` stats sampleID    --  print stats from blast, trimming and RUM\n"
+ngsUsage_STATS="Usage: `basename $0` stats OPTIONS sampleID    --  print stats from blast, trimming and aligner\n"
 
 ##########################################################################################
 # HELP TEXT
 ##########################################################################################
 
-ngsHelp_STATS="Usage:\n\t`basename $0` stats sampleID\n\n"
-ngsHelp_STATS+="Prints out BLAST, TRIM, and RUM stats. The stats will be tab delimited so they can be copy-pasted into an Excel table."
+ngsHelp_STATS="Usage:\n\t`basename $0` stats [-a aligner] sampleID\n\n"
+ngsHelp_STATS+="Input:\n\tsampleID/blast/speciesCounts.txt\n\tsampleID/trim/stats.txt\n\tsampleID/star/Log.final.out\n"
+ngsHelp_STATS+="Output:\n\tprinting to console\n"
+ngsHelp_STATS+="Options:\n"
+ngsHelp_STATS+="\t-a aligner - which aligner log file to parse for stats (default: star). Choices are 'star' or 'rum'.\n"
+ngsHelp_STATS+="Prints out BLAST, TRIM, and STAR/RUM stats. The stats will be tab delimited so they can be copy-pasted into an Excel table. This will not write to the analysis.log file."
+
+##########################################################################################
+# LOCAL VARIABLES WITH DEFAULT VALUES. Using the naming convention to
+# make sure these variables don't collide with the other modules.
+##########################################################################################
+
+ngsLocal_STATS_ALIGNER="star"
 
 ##########################################################################################
 # PROCESSING COMMAND LINE ARGUMENTS
@@ -41,9 +52,23 @@ ngsArgs_STATS() {
 	if [ $# -lt 1 ]; then
 		printHelp $COMMAND
 		exit 0
-	else
-		SAMPLE=$1
 	fi
+
+	# getopts doesn't allow for optional arguments so handle them manually
+	while true; do
+		case $1 in
+			-a) ngsLocal_STATS_ALIGNER=$2
+				shift; shift;
+				;;
+			-*) printf "Illegal option: '%s'\n" "$1"
+				printHelp $COMMAND
+				exit 0
+				;;
+ 			*) break ;;
+		esac
+	done
+	
+	SAMPLE=$1
 }
 
 ##########################################################################################
@@ -52,45 +77,92 @@ ngsArgs_STATS() {
 ##########################################################################################
 
 ngsCmd_STATS() {
-	prnCmd "# BEGIN: STATS"
+	# don't write to log file as this is just printing stats
+	echo -e "\n##################################################################"
+	echo "# BEGIN: STATS"
+	echo -ne `date`
+	echo -ne "  "
+	echo $SAMPLE
+	echo "##################################################################"
 	
 	echo "-- BLAST --"
 	cat $SAMPLE/blast/speciesCounts.txt; 
 
 	# the last line of the speciesCounts.txt file is the following tab-delimited list of values:
 	# Total Hits	Hits Not Counted	Bacteria	Fish	Fly	Human	Mouse	Rat	Yeast
-	BLASTFIELDS=`tail -1 $SAMPLE/blast/speciesCounts.txt`
+	BLAST_HEADER=`tail -2 $SAMPLE/blast/speciesCounts.txt | head -1`
+	BLAST_VALUES=`tail -1 $SAMPLE/blast/speciesCounts.txt`
 	
-	echo -e "\n-- Adapter Trimming --"
-	cat $SAMPLE/trimAdapter.stats.txt; 
+	echo -e "\n-- Trimming --"
+	cat $SAMPLE/trim/stats.txt; 
 
-	NUMREADS=`grep "total reads processed" $SAMPLE/trimAdapter.stats.txt | awk '{print $1}'`
-	TRIMAD_K=`grep "trimmed and kept" $SAMPLE/trimAdapter.stats.txt | awk '{print $1}'`
-	TRIMAD_D=`grep "discarded with final" $SAMPLE/trimAdapter.stats.txt | awk '{print $1}'`
+	TRIM_HEADER=`tail -2 $SAMPLE/trim/stats.txt | head -1`
+	TRIM_VALUES=`tail -1 $SAMPLE/trim/stats.txt`
+
+	if [ "$ngsLocal_STATS_ALIGNER" = "rum" ]; then
+		echo -e "\n-- RUM --"
+		#head -32 $SAMPLE/rum.trim/mapping_stats.txt
+
+		# we aren't currently using NUMTRIM. For now it's just to flag whether or not the sample is single- or paired-end.
+		numTrim=`head -32 $SAMPLE/rum.trim/mapping_stats.txt | grep "Number of read pairs:" | awk '{print $5}'`
+
+		rumAli=`head -32 $SAMPLE/rum.trim/mapping_stats.txt | tail -10 | grep "At least one" | awk '{print $10}' | tr -d '()'`
+		$RUM_HEADER="RUM Total Aligned"
+		$RUM_VALUES="$rumAli"
 	
-	echo -e "\n-- Poly A/T Trimming --"
-	cat $SAMPLE/trimPolyAT.stats.txt; 
-	
-	TRIMAT_K=`grep "trimmed and kept" $SAMPLE/trimPolyAT.stats.txt | awk '{print $1}'`
-	TRIMAT_D=`grep "discarded with final" $SAMPLE/trimPolyAT.stats.txt | awk '{print $1}'`
-	
-	echo -e "\n-- RUM --"
-	head -32 $SAMPLE/rum.trim/mapping_stats.txt
-	
-	NUMTRIM=`head -32 $SAMPLE/rum.trim/mapping_stats.txt | grep "Number of read pairs:" | awk '{print $5}'`
-	RUMALI=`head -32 $SAMPLE/rum.trim/mapping_stats.txt | tail -10 | grep "At least one" | awk '{print $10}' | tr -d '()'`
-	
-	# if $NUMTRIM is empty then probably single-end, which has different stats in mapping_stats.txt file
-	if [ ! "$NUMTRIM" ]; then
-		NUMTRIM=`head -20 $SAMPLE/rum.trim/mapping_stats.txt | grep "Number of reads:" | awk '{print $4}'`
-		RUMALI=`head -20 $SAMPLE/rum.trim/mapping_stats.txt | grep "TOTAL:" | awk '{print $3}' | tr -d '()'`
-		echo -e "\nPROCESSING RUM AS SINGLE-END"
+		# if $NUMTRIM is empty then probably single-end, which has different stats in mapping_stats.txt file
+		if [ ! "$numTrim" ]; then
+			echo -e "\nPROCESSING RUM AS SINGLE-END"
+
+			rumAli=`head -20 $SAMPLE/rum.trim/mapping_stats.txt | grep "TOTAL:" | awk '{print $3}' | tr -d '()'`
+			$RUM_HEADER="RUM Total Aligned"
+			$RUM_VALUES="$rumAli"
+		fi
+
+	elif [ "$ngsLocal_STATS_ALIGNER" = "star" ]; then
+		echo -e "\n-- STAR --"
+
+		avgReadLen=`grep "Average input read length" $SAMPLE/star/Log.final.out | awk -F $'\t' '{print $2}'`
+		STAR_HEADER="Avg Inp Read Len"
+		STAR_VALUES="$avgReadLen"
+
+		avgMapLen=`grep "Average mapped length" $SAMPLE/star/Log.final.out | awk -F $'\t' '{print $2}'`
+		STAR_HEADER="$STAR_HEADER\tAvg Uniq Map Len"
+		STAR_VALUES="$STAR_VALUES\t$avgMapLen"
+
+		uniqMap=`grep "Uniquely mapped reads %" $SAMPLE/star/Log.final.out | awk -F $'\t' '{print $2}'`
+		STAR_HEADER="$STAR_HEADER\tUniq Map"
+		STAR_VALUES="$STAR_VALUES\t$uniqMap"
+
+		multimapped=`grep "% of reads mapped to multiple loci" $SAMPLE/star/Log.final.out | awk -F $'\t' '{print $2}'`
+		STAR_HEADER="$STAR_HEADER\tMultimapped"
+		STAR_VALUES="$STAR_VALUES\t$multimapped"
+
+		tooShort=`grep "% of reads unmapped: too short" $SAMPLE/star/Log.final.out | awk -F $'\t' '{print $2}'`
+		STAR_HEADER="$STAR_HEADER\tToo Short"
+		STAR_VALUES="$STAR_VALUES\t$tooShort"
+	fi
+
+	echo -e "\n-- $SAMPLE --"
+
+	if [ "$ngsLocal_STATS_ALIGNER" = "rum" ]; then
+		echo -e "$BLAST_HEADER\t$TRIM_HEADER\t$RUM_HEADER"
+		echo -e "$BLAST_VALUES\t$TRIM_VALUES\t$RUM_VALUES"
+
+	elif [ "$ngsLocal_STATS_ALIGNER" = "star" ]; then
+		echo -e "$BLAST_HEADER\t$TRIM_HEADER\t$STAR_HEADER"
+		echo -e "$BLAST_VALUES\t$TRIM_VALUES\t$STAR_VALUES"
+
+	else
+		echo -e "$BLAST_HEADER\t$TRIM_HEADER"
+		echo -e "$BLAST_VALUES\t$TRIM_VALUES"
 	fi
 	
-	echo -e "\n-- $SAMPLE --"
-	echo -e "Num Reads\tNum Reads Trimmed\tRUM\tTotal Hits\tHits Not Counted\tBacteria\tFish\tFly\tHuman\tMouse\tRat\tYeast\tBowtie\tAdapt Trim Kept\tAdapt Trim Disc\tPolyAT Trim Kept\tPolyAT Trim Disc\tDate"
-	# extra tab between blast and trimming to account for lacking bowtie stats
-	echo -e "$NUMREADS\t$NUMTRIM\t$RUMALI\t$BLASTFIELDS\t\t$TRIMAD_K\t$TRIMAD_D\t$TRIMAT_K\t$TRIMAT_D\t$(date +%m/%d/%Y)\n"
-	
-	prnCmd "# FINISHED: STATS"
+	# don't write to log file as this is just printing stats
+	echo -e "\n##################################################################"
+	echo "# FINISHED: STATS"
+	echo -ne `date`
+	echo -ne "  "
+	echo $SAMPLE
+	echo "##################################################################"
 }
