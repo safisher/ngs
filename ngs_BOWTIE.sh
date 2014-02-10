de20b5462e2419c33046ba80fb626ce987e156e7
Stephen Fisher <safisher@sas.upenn.edu>
2014-02-10 15:15:27 -0500

    New BOWTIE module
    Added a lot of arguments.
    Changed the naming convention for the output file.

--- ngs_BOWTIE.sh
+++ ngs_BOWTIE.sh
@@ -16,11 +16,11 @@
 
 ##########################################################################################
 # SINGLE-END READS:
-# INPUT: $SAMPLE/orig/unaligned_1.fq
+# INPUT: $SAMPLE/trim/unaligned_1.fq
 # OUTPUT: $SAMPLE/bowtie/bowtie-sorted.bam and $SAMPLE/bowtie/stats.txt
 #
 # PAIRED-END READS:
-# INPUT: $SAMPLE/orig/unaligned_1.fq and $SAMPLE/orig/unaligned_2.fq
+# INPUT: $SAMPLE/trim/unaligned_1.fq and $SAMPLE/orig/unaligned_2.fq
 # OUTPUT: $SAMPLE/bowtie/bowtie-sorted.bam and $SAMPLE/bowtie/stats.txt
 #
 # REQUIRES: bowtie, samtools
@@ -30,26 +30,35 @@
 # USAGE
 ##########################################################################################
 
-ngsUsage_BOWTIE="Usage: `basename $0` bowtie OPTIONS sampleID    --  run bowtie on untrimmed reads\n"
+ngsUsage_BOWTIE="Usage: `basename $0` bowtie OPTIONS sampleID    --  run bowtie on trimmed reads\n"
 
 ##########################################################################################
 # HELP TEXT
 ##########################################################################################
 
-ngsHelp_BOWTIE="Usage:\n\t`basename $0` bowtie -p numProc -s species [-se] sampleID\n"
-ngsHelp_BOWTIE+="Input:\n\tsampleID/orig/unaligned_1.fq\n\tsampleID/orig/unaligned_2.fq (paired-end reads)"
-ngsHelp_BOWTIE+="Output:\n\tsampleID/bowtie/bowtie-sorted.bam\n\tsampleID/bowtie/stats.txt\n"
+ngsHelp_BOWTIE="Usage:\n\t`basename $0` bowtie [-i inputDir] -v mismatches -m maxMulti -ins insertSize -p numProc -s species [-se] sampleID\n"
+ngsHelp_BOWTIE+="Input:\n\tsampleID/inputDir/unaligned_1.fq\n\tsampleID/inputDir/unaligned_2.fq (paired-end reads)\n"
+ngsHelp_BOWTIE+="Output:\n\tsampleID/bowtie/sampleID.sorted.bam\n\tsampleID/bowtie/sampleID.suppressed.sorted.bam\n\tsampleID/bowtie/sampleID.stats.txt\n"
 ngsHelp_BOWTIE+="Requires:\n\tbowtie ( http://bowtie-bio.sourceforge.net/index.shtml )\n\tsamtools ( http://samtools.sourceforge.net/ )\n"
 ngsHelp_BOWTIE+="Options:\n"
+ngsHelp_BOWTIE+="\t-i inputDir - directory with unaligned reads (default: trim)\n"
+ngsHelp_BOWTIE+="\t-v mismatches - maximum mismatches allowed per read length (default: 3)\n"
+ngsHelp_BOWTIE+="\t-m maxMulti - suppress all alignments if > m alignments (default: 1)\n"
+ngsHelp_BOWTIE+="\t-ins insertSize - insert size for PE alignment (default: 350bp)\n"
 ngsHelp_BOWTIE+="\t-p numProc - number of cpu to use\n"
 ngsHelp_BOWTIE+="\t-s species - species from repository: $BOWTIE_REPO\n"
 ngsHelp_BOWTIE+="\t-se - single-end reads (default: paired-end)\n\n"
-ngsHelp_BOWTIE+="Run bowtie on the original, untrimmed data (ie sampleID/orig). Output is placed in the directory sampleID/bowtie."
+ngsHelp_BOWTIE+="Run bowtie on the unaligned reads (ie sampleID/inputDir). The arguments used assume Bowtie version 1. Output is placed in the directory sampleID/bowtie. Multimapping reads that exceed the maxMulti count are output to the sampleID.suppressed.sorted.bam file (ie the -max flag is used to direct the reads to this file)."
 
 ##########################################################################################
 # LOCAL VARIABLES WITH DEFAULT VALUES. Using the naming convention to
 # make sure these variables don't collide with the other modules.
 ##########################################################################################
+
+ngsLocal_BOWTIE_INP_DIR="trim"
+ngsLocal_BOWTIE_MISMATCHES=3
+ngsLocal_BOWTIE_MAXMULTI=1
+ngsLocal_BOWTIE_INSERTSIZE=350
 
 ##########################################################################################
 # PROCESSING COMMAND LINE ARGUMENTS
@@ -57,7 +66,7 @@ ngsHelp_BOWTIE+="Run bowtie on the original, untrimmed data (ie sampleID/orig). 
 ##########################################################################################
 
 ngsArgs_BOWTIE() {
-	if [ $# -lt 5 ]; then
+	if [ $# -lt 8 ]; then
 		printHelp $COMMAND
 		exit 0
 	fi
@@ -65,6 +74,18 @@ ngsArgs_BOWTIE() {
    	# getopts doesn't allow for optional arguments so handle them manually
 	while true; do
 		case $1 in
+			-i) ngsLocal_BOWTIE_INP_DIR=$2
+				shift; shift;
+				;;
+			-v) ngsLocal_BOWTIE_MISMATCHES=$2
+				shift; shift;
+				;;
+			-m) ngsLocal_BOWTIE_MAXMULTI=$2
+				shift; shift;
+				;;			
+			-ins) ngsLocal_BOWTIE_INSERTSIZE=$2
+				shift; shift;
+				;;
 			-p) NUMCPU=$2
 				shift; shift;
 				;;
@@ -105,15 +126,19 @@ ngsCmd_BOWTIE() {
 	
 	if $SE; then 
         # single-end
-		prnCmd "bowtie -t -v 3 -a -m 10 --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES $SAMPLE/orig/unaligned_1.fq $SAMPLE/bowtie/output_p.sam > $SAMPLE/bowtie/stats.txt 2>&1"
+		prnCmd "bowtie -t -v $ngsLocal_BOWTIE_MISMATCHES -a -m $ngsLocal_BOWTIE_MAXMULTI --best --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES $SAMPLE/$ngsLocal_BOWTIE_INP_DIR/unaligned_1.fq $SAMPLE/bowtie/output_p.sam --max $SAMPLE/bowtie/suppressed.fq --un $SAMPLE/bowtie/unmapped.fq > $SAMPLE/bowtie/$SAMPLE.stats.txt 2>&1"
 		if ! $DEBUG; then 
-			bowtie -t -v 3 -a -m 10 --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES $SAMPLE/orig/unaligned_1.fq $SAMPLE/bowtie/output_p.sam > $SAMPLE/bowtie/stats.txt 2>&1
+			bowtie -t -v $ngsLocal_BOWTIE_MISMATCHES -a -m $ngsLocal_BOWTIE_MAXMULTI --best --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES $SAMPLE/$ngsLocal_BOWTIE_INP_DIR/unaligned_1.fq $SAMPLE/bowtie/output_p.sam --max $SAMPLE/bowtie/suppressed.fq --un $SAMPLE/bowtie/unmapped.fq > $SAMPLE/bowtie/$SAMPLE.stats.txt 2>&1
 		fi
 	else 
         # paired-end
-		prnCmd "bowtie -t -v 3 -a -m 10 --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES -1 $SAMPLE/orig/unaligned_1.fq -2 $SAMPLE/orig/unaligned_2.fq $SAMPLE/bowtie/output_p.sam > $SAMPLE/bowtie/stats.txt 2>&1"
+		let "MININST=$ngsLocal_BOWTIE_INSERTSIZE - 100"
+		#MININST="250"
+		let "MAXINST=$ngsLocal_BOWTIE_INSERTSIZE + 100"
+		#MAXINST="450"
+		prnCmd "bowtie -t -v $ngsLocal_BOWTIE_MISMATCHES --minins $MININST --maxins $MAXINST -a -m $ngsLocal_BOWTIE_MAXMULTI --best --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES -1 $SAMPLE/$ngsLocal_BOWTIE_INP_DIR/unaligned_1.fq -2 $SAMPLE/$ngsLocal_BOWTIE_INP_DIR/unaligned_2.fq $SAMPLE/bowtie/output_p.sam --max $SAMPLE/bowtie/suppressed.sam --un $SAMPLE/bowtie/unmapped.fq > $SAMPLE/bowtie/$SAMPLE.stats.txt 2>&1"
 		if ! $DEBUG; then 
-			bowtie -t -v 3 -a -m 10 --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES -1 $SAMPLE/orig/unaligned_1.fq -2 $SAMPLE/orig/unaligned_2.fq $SAMPLE/bowtie/output_p.sam > $SAMPLE/bowtie/stats.txt 2>&1
+			bowtie -t -v $ngsLocal_BOWTIE_MISMATCHES --minins $MININST --maxins $MAXINST -a -m $ngsLocal_BOWTIE_MAXMULTI --best --sam -p $NUMCPU $BOWTIE_REPO/$SPECIES -1 $SAMPLE/$ngsLocal_BOWTIE_INP_DIR/unaligned_1.fq -2 $SAMPLE/$ngsLocal_BOWTIE_INP_DIR/unaligned_2.fq $SAMPLE/bowtie/output_p.sam --max $SAMPLE/bowtie/suppressed.sam --un $SAMPLE/bowtie/unmapped.fq > $SAMPLE/bowtie/$SAMPLE.stats.txt 2>&1
 		fi
 	fi
 	
@@ -137,11 +162,20 @@ ngsCmd_BOWTIE() {
 	prnCmd "samtools view -h -b -S -o output_p.bam output_p.sam"
 	if ! $DEBUG; then samtools view -h -b -S -o output_p.bam output_p.sam; fi
 	prnCmd "samtools sort output_p.bam bowtie-sorted"
-	if ! $DEBUG; then samtools sort output_p.bam bowtie-sorted; fi
-	prnCmd "samtools index bowtie-sorted.bam"
-	if ! $DEBUG; then samtools index bowtie-sorted.bam; fi
+	if ! $DEBUG; then samtools sort output_p.bam $SAMPLE.sorted; fi
+	prnCmd "samtools index $SAMPLE.sorted.bam"
+	if ! $DEBUG; then samtools index $SAMPLE.sorted.bam; fi
 	prnCmd "rm output_p.sam output_p.bam"
 	if ! $DEBUG; then rm output_p.sam output_p.bam; fi
+	
+	prnCmd "samtools view -h -b -S -o suppressed.bam suppressed.sam"
+	if ! $DEBUG; then samtools view -h -b -S -o suppressed.bam suppressed.sam; fi
+	prnCmd "samtools sort suppressed.bam $SAMPLE.suppressed.sorted"
+	if ! $DEBUG; then samtools sort suppressed.bam $SAMPLE.suppressed.sorted; fi
+	prnCmd "samtools index $SAMPLE.suppressed.sorted.bam"
+	if ! $DEBUG; then samtools index $SAMPLE.suppressed.sorted.bam; fi
+	prnCmd "rm suppressed.sam suppressed.bam"
+	if ! $DEBUG; then rm suppressed.sam suppressed.bam; fi
 	
     # return to proper directory and restore $JOURNAL
 	prnCmd "cd $CUR_DIR"
