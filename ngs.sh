b6a57374e43783e62d97b45332880b4c93877332
Stephen Fisher <safisher@sas.upenn.edu>
2014-03-20 12:14:21 -0400

    added k-mer search to BLAST
    * BLAST has optional k-mer search which will count the number of times
    the k-mer is present in the reads that didn’t map with blast. The
    output will be included in the BLAST stats.
    * added option to change the number of reads used by blast search.

--- ngs.sh
+++ ngs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# Copyright (c) 2012,2013, Stephen Fisher and Junhyong Kim, University of
+# Copyright (c) 2012-2014 Stephen Fisher and Junhyong Kim, University of
 # Pennsylvania.  All Rights Reserved.
 #
 # You may not use this file except in compliance with the Kim Lab License
