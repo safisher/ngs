ad44d494729a29f6dc2d45387c6e2f8c5792c640
Stephen Fisher <safisher@sas.upenn.edu>
2015-04-07 15:02:09 -0400

    more careful about what directories it will recurse into

--- ancillary/rename_sample.sh
+++ ancillary/rename_sample.sh
@@ -37,44 +37,50 @@ renameOther() {
 }
 
 
-
-pushd analyzed/$FROM
-for dir in *; do
-    case $dir in
-	log)
-	    for logfile in log/*.log; do
-		sed "s/$FROM/$TO/g" $logfile > $logfile.tmp
-		mv $logfile.tmp $logfile
-	    done
-	    ;;
-	star)
-	    rename "$FROM" "$TO" star/*
-	    sed "s/$FROM/$TO/g" star/Log.out > star/Log.tmp
-	    mv star/Log.tmp star/Log.out
-	    ;;
-	htseq)
-	    rename "$FROM" "$TO" htseq/*
-	    ;;
-	blast)
-	    rename "$FROM" "$TO" blast/*
-	    ;;
-	fastqc)
-	    rename "$FROM" "$TO" fastqc/*
-	    ;;
-	trim)
-	    rename "$FROM" "$TO" trim/*
-	    ;;
-	blastdb)
-	    rename "$FROM" "$TO" blastdb/*
-	    ;;
-	*)
-	    if [[ -d $dir ]]; then
-		renameOther $dir
-	    fi
-	    ;;
-    esac
+for folder in pipeline.v1 analyzed; do
+    if ! pushd $folder/$FROM; then
+	echo "$folder/$FROM not found"
+	continue
+    fi
+    for dir in *; do
+	case $dir in
+	    log)
+		for logfile in log/*.log; do
+		    sed "s/$FROM/$TO/g" $logfile > $logfile.tmp
+		    mv $logfile.tmp $logfile
+		echo `date` >> Changed_${FROM}_to_${TO}.txt
+		done
+		;;
+	    star)
+		rename "$FROM" "$TO" star/*
+		sed "s/$FROM/$TO/g" star/Log.out > star/Log.tmp
+		mv star/Log.tmp star/Log.out
+		;;
+	    htseq)
+		rename "$FROM" "$TO" htseq/*
+		;;
+	    blast)
+		rename "$FROM" "$TO" blast/*
+		;;
+	    fastqc)
+		rename "$FROM" "$TO" fastqc/*
+		;;
+	    trim)
+		rename "$FROM" "$TO" trim/*
+		;;
+	    blastdb)
+		rename "$FROM" "$TO" blastdb/*
+		;;
+	    *)
+		if [[ -d $dir ]]; then
+		    renameOther $dir
+		fi
+		;;
+	esac
+    done
+    popd
+    mv $folder/$FROM $folder/$TO
 done
-popd
-mv analyzed/$FROM analyzed/$TO
+
 mv raw/$FROM raw/$TO
 
